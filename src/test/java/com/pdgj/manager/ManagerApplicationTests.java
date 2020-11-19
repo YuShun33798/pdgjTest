0c6c88fa9ce355579dc1aac999eb912ca151fb76
YuShun33798 <13914733798@126.com>
2020-11-19 16:42:09 +0800

    提交文件

---
+++ src/test/java/com/pdgj/manager/ManagerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.pdgj.manager;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
